1b278a73b5fe33be4350d3b2f3bcc43d8eb9c0cb
Oladimeji Odusote <taiwo.odusote@yahoo.com>
2025-06-14 20:44:00 +0100

    Deployment v1

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 export default defineConfig({
-  base: '/<your-repo-name>/', // ← Replace with your repo name
+  base: '/grocery-landing-page/', // ← Replace with your repo name
   plugins: [react()],
 })
